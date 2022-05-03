2027fbbb4d6cc5a618befb3430c8414826b9c39a
J. Basten <61888847+kamexX@users.noreply.github.com>
2022-05-03 18:13:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+   name='Pdf Splitter',
+   version='1.0',
+   description='Utility to split a PDF into several chapter',
+   author='KamexX',
+   packages=['pdf_splitter'], 
+   install_requires=['pypdf2']
+)
