aa492222da363c66e675fcbacde106452eb86501
J. Basten <61888847+kamexX@users.noreply.github.com>
2022-05-03 19:08:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-   name='Pdf Splitter',
+   name='pdfsplit',
    version='1.1',
    description='Utility to split a PDF into several chapter',
    author='KamexX',
